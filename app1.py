f5b0e22dba2bd968233d27f0b308fe95c429d0fb
MuzammilAarif <muzammilaarif12@gmail.com>
2021-08-25 19:12:26 +0530

    Screenshot Version1.0

--- app1.py
+++ app1.py
@@ -1,12 +1,8 @@
-# from types import MethodType
-# from app import URL1
 from flask import Flask, request, render_template
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 app = Flask(__name__)
-
 
 @app.route("/")
 def fond():
@@ -17,24 +13,18 @@ def fond():
 def find():
     
     URL = request.form.get('search')
-    # URL1 = str(URL)
-    # print(URL)
-    # print(type(URL))
-    # print(type(URL1))
-    # URL = 'https://github.com/MuzammilAarif/docker-compose-flask-math'
     options = webdriver.ChromeOptions()
     options.headless = True
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
     driver.get(URL)
+    
     S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
-    driver.set_window_size(S('Width'),S('Height')) # May need manual adjustment
+    driver.set_window_size(S('Width'),S('Height'))
     driver.find_element_by_tag_name('body').screenshot('web_screenshot.png')
     driver.quit()
 
-
-
     return render_template("screenshot.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
